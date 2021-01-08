342c1df3bde228ec48a66e380c9beae6f8b75c54
canokcelik <canokcelik@hotmail.com>
2021-01-08 14:43:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   echo "CPU performans izleme icin yuk olusturan kod.";
 
   $x = 0.0001;
-  for ($i = 0; $i <= 1000000; $i++) {
+  for ($i = 0; $i <= 10000000; $i++) {
     $x += sqrt($x);
   }
   echo "OK!";
